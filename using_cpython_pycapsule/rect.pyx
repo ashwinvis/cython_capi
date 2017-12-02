8388d8fd883695ad68ab2bc23e5f4d3b335634ab
Ashwin Vishnu <ashwinvis@protonmail.com>
2017-12-02 15:55:26 +0100

    Make capsules for class methods manually

---
+++ using_cpython_pycapsule/rect.pyx
@@ -0,0 +1,55 @@
+from cpython.pycapsule cimport PyCapsule_New
+
+
+cdef extern from "Rectangle.h" namespace "shapes":
+    cdef cppclass Rectangle:
+        Rectangle() except +
+        Rectangle(int, int, int, int) except +
+        int x0, y0, x1, y1
+        int getArea()
+        void getSize(int* width, int* height)
+        void move(int, int)
+
+
+cdef int foo(int x):
+    return 2 * x
+
+
+cpdef get_capi():
+    cap = PyCapsule_New(<void *>foo, 'int (int)', NULL)
+    return cap
+
+
+cdef class PyRectangle:
+    cdef Rectangle c_rect      # hold a C++ instance which we're wrapping
+    cdef dict __capi__
+
+    def __cinit__(self, int x0, int y0, int x1, int y1):
+        self.c_rect = Rectangle(x0, y0, x1, y1)
+
+    cpdef get_capi(self):
+        self.__capi__ = dict(
+            get_area=PyCapsule_New(<void *>self.get_area, 'int (int)', NULL),
+            twice=PyCapsule_New(<void *>self.twice, 'int (int)', NULL),
+            twice_static=PyCapsule_New(<void *>self.twice_static, 'int (int)', NULL)
+        )
+        return self.__capi__
+
+    cdef int get_area(self, int c):
+        c = c * 2
+        return self.c_rect.getArea()
+
+    cdef int twice(self, int c):
+        return c * 2
+
+    @staticmethod
+    cdef int twice_static(int c):
+        return c * 2
+
+    def get_size(self):
+        cdef int width, height
+        self.c_rect.getSize(&width, &height)
+        return width, height
+
+    def move(self, dx, dy):
+        self.c_rect.move(dx, dy)
---
+++ using_cpython_pycapsule/rect_pythran.py
@@ -0,0 +1,4 @@
+# pythran export get_area(int (int), int)
+def get_area(cy_get_area, c):
+    r = cy_get_area(c)
+    return r
